d04268c274c1bdb6fc034093be91a70f68e8371c
liquidddate <edavidson4265@gmail.com>
2026-09-05 21:09:00 -0700

    Initial commit

---
+++ Chapter_1.py
@@ -0,0 +1,23 @@
+#1.9.2
+print(round(42.5))
+print(round(43.5))
+#1.9.3
+print(2++2)
+print(4, 2)
+print(round(42.5))
+#1.9.4
+print(type(765))
+print(type(2.718))
+print(type('2 pi'))
+print(type(abs(-7)))
+print(type(abs(-7.0)))
+print(type(abs))
+print(type(int))
+print(type(type))
+#1.9.5
+seconds = (42 * 60 + 42)
+miles = 10 / 1.61
+print(seconds / miles)
+seconds_per_mile = 412.5
+print(round(seconds_per_mile / 60, 2))
+print(miles / (seconds / 3600))
---
+++ Chapter_2.py
@@ -0,0 +1,21 @@
+#2.11.2
+n = 17
+x = y = 1
+print("peepeepoopoo");
+print("poopoopeepeeperiod")
+import math #no module named maath
+
+#2.11.3
+radius = 5
+volume = (4 / 3) * math.pi * radius ** 3
+
+print(f"The volume of a sphere with radius {radius} in CM is {volume} in cubic CM")
+
+x = 42
+cos_x = math.cos(x)
+sin_x = math.sin(x)
+print((cos_x ** 2) + (sin_x ** 2))
+
+print(math.e ** 2)
+print(math.pow(math.e, 2))
+print(math.exp(2))
